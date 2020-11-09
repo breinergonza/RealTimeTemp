5ba37c503a2b01f565f26cf2c9107f07ee6efb1d
Breitner <breinergonza@hotmail.com>
2020-11-09 11:19:56 -0500

    Servidor

--- api_in_node/index.ts
+++ api_in_node/index.ts
@@ -18,7 +18,7 @@ server.app.use( cors({ origin: true, credentials: true  }) );
 server.app.use('/', router );
 
 server.start( () => {
-    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+    console.log(`Servidor ${process.env.NODE_ENV}, corriendo en el puerto ${ server.port }`);
 });
 
 
