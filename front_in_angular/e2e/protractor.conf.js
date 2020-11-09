d416004488e51a1640d69730d1c00973ee4b7780
Breitner <breinergonza@hotmail.com>
2020-11-09 11:26:55 -0500

    Ajuste url

--- front_in_angular/e2e/protractor.conf.js
+++ front_in_angular/e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://52.23.188.230:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
--- front_in_angular/src/app/app.module.ts
+++ front_in_angular/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
-const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+import { environment } from '../environments/environment';
+
+const config: SocketIoConfig = { url: environment.url, options: {} };
 
 import { ChartsModule } from 'ng2-charts';
 import { HttpClientModule } from '@angular/common/http';
